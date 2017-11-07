180e594087294d7b08b893e417388b032602a8c7
Stiven Ramírez Arango <you@example.com>
2017-11-06 23:45:27 -0500

    Demo

--- serverExpress.js
+++ serverExpress.js
@@ -1,47 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var methodOverride = require("method-override");
 var app = express();
 
-// Connection to DB
-mongoose.connect('mongodb://https://biblioteca-movil.herokuapp.com/books', function(err, res) {
- if(err) throw err;
- console.log('Connected to Database');
-});
-
-// Middlewares
-app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json()); 
-app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 
-// Import Models and Controllers
-var models = require('./models/book')(app, mongoose);
-var ClientCtrl = require('./controllers/books');
-
-var router = express.Router();
-
 // Index - Route
-router.get('/', function(req, res) { 
+app.get('/', function(req, res) { 
  res.send("Proyecto Integrador 1 - Biblioteca Móvil EAFIT");
 });
-
-app.use(router);
-
-// API routes
-var api = express.Router();
-
-api.route('/books') 
- .get(ClientCtrl.findAll)
- .post(ClientCtrl.add);
-
-api.route('/books/:id') 
- .get(ClientCtrl.findById)
- .put(ClientCtrl.update)
- .delete(ClientCtrl.delete);
-
-app.use('/eafit/biblioteca-movil', api);
 
 // Start server
 app.listen(process.env.PORT, function() {
