25cd2f66b712479f2002dc0a691c73479e631d5a
Stiven Ramírez Arango <you@example.com>
2017-11-14 22:44:14 -0500

    Modificando paquetes

--- serverExpress.js
+++ serverExpress.js
@@ -26,7 +26,7 @@ var Book = require('./controllers/books');
 var router = express.Router();
 // Index - Route
 app.get('/', function(req, res) { 
- res.send("Biblioteca Móvil EAFIT - Proyecto Integrador 1");
+ res.send("Biblioteca Móvil EAFIT - Proyecto Integrador 1 (El Mago)");
 });
 
 app.use(router);
