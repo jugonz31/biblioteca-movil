bf87b8e88299aa6ae2f3b89bfca91f3a1f8fc6a4
Stiven Ramírez Arango <you@example.com>
2017-11-15 20:04:23 -0500

    Añadiendo Heroku Server v10.0

--- controllers/books.js
+++ controllers/books.js
@@ -5,7 +5,7 @@ var Book = mongoose.model('Book');
 exports.findAll = function(req, res) {
  Book.find(function(err, books) {
  if(err) res.send(500, err.message);
- console.log('GET /books3')
+ console.log('GET /books')
  res.status(200).jsonp(books);
  });
 };
